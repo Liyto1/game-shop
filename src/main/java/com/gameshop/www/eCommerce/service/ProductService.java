d1344f962239a2b12fd7cafaae3db891f1f29e1f
Andrii <128421118+Dvi55@users.noreply.github.com>
2023-12-16 22:17:37 +0200

    Merge pull request #5
    * ON-137: add endpoint to displaying recently added products

--- src/main/java/com/gameshop/www/eCommerce/service/ProductService.java
+++ src/main/java/com/gameshop/www/eCommerce/service/ProductService.java
@@ -4,6 +4,7 @@ import com.gameshop.www.eCommerce.dao.ProductDAO;
 import com.gameshop.www.eCommerce.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,4 +25,8 @@ public class ProductService {
         return productDAO.findByCategory_NameIgnoreCase(name, pageRequest);
     }
 
+    public Page<Product> getRecentlyAddProducts(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Order.desc("createdAt")));
+        return productDAO.findAll(pageRequest);
+    }
 }
