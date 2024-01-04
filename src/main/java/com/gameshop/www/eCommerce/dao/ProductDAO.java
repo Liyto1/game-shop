0e9bd3e6ca63f00f3ccc43f06b0c7322dbbbca76
Andrii <128421118+Dvi55@users.noreply.github.com>
2024-01-04 13:55:54 +0200

    Feature/on 142 autocomplete search (#7)
    * Dev: initialize Java project
    
    * Dev: initialize Java project
    
    * Dev: add connection to database
    
    * Merge pull request #1
    
    * Auth: add endpoint for register and login. Add jwt token
    
    * Auth: add endpoint which return data from jwt token. Add JWTRequestFi…
    
    * DB-arch:create a database structure (#2)
    
    Co-authored-by: Andrey <kisluna789@gmail.com>
    
    * DB-arch:fix architecture (#3)
    
    Co-authored-by: Andrey <kisluna789@gmail.com>
    
    * Feature/ON-136: displaying products by category (#4)
    
    * ON-136: add endpoint to displaying product by category
    
    * ON-136: add endpoint to displaying product by category
    
    * ON-136: add endpoint to displaying paginating products
    
    ---------
    
    Co-authored-by: Andrey <kisluna789@gmail.com>
    Co-authored-by: Andrii <andrii.kislun@gmail.com>
    
    * Merge pull request #5
    
    * ON-137: add endpoint to displaying recently added products
    
    * ON-142: add endpoint to search with suggestions
    
    * ON-142: add endpoint to search with suggestions
    
    ---------
    
    Co-authored-by: Andrey <kisluna789@gmail.com>
    Co-authored-by: Andrii <andrii.kislun@gmail.com>

--- src/main/java/com/gameshop/www/eCommerce/dao/ProductDAO.java
+++ src/main/java/com/gameshop/www/eCommerce/dao/ProductDAO.java
@@ -6,6 +6,7 @@ import com.gameshop.www.eCommerce.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
--- src/main/java/com/gameshop/www/eCommerce/model/Brand.java
+++ src/main/java/com/gameshop/www/eCommerce/model/Brand.java
@@ -1,5 +1,9 @@
 package com.gameshop.www.eCommerce.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +15,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "brand")
 public class Brand {
+  
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
