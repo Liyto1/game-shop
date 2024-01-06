4710785a2326a518fbb8224ea70b54f2365eb78b
Andrey <kisluna789@gmail.com>
2024-01-06 14:54:14 +0200

    fix: fix expiring jwt-token

--- src/main/java/com/gameshop/www/eCommerce/service/JWTService.java
+++ src/main/java/com/gameshop/www/eCommerce/service/JWTService.java
@@ -2,6 +2,8 @@ package com.gameshop.www.eCommerce.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import com.gameshop.www.eCommerce.model.LocalUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,4 +39,20 @@ public class JWTService {
     public String getUserEmail(String token) {
         return JWT.decode(token).getClaim(USER_EMAIL).asString();
     }
+
+    public boolean isTokenValid(String token) {
+        try {
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(issuer)
+                    .build();
+
+            DecodedJWT jwt = verifier.verify(token);
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
