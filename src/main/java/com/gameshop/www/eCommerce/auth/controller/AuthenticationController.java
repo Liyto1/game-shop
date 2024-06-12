3f81dbe60e47c311c48d559b9e2634608ae54365
Andrii <kisluna789@gmail.com>
2024-06-12 18:12:48 +0300

    BugFix: fix image in generated products

--- src/main/java/com/gameshop/www/eCommerce/auth/controller/AuthenticationController.java
+++ src/main/java/com/gameshop/www/eCommerce/auth/controller/AuthenticationController.java
@@ -15,13 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -43,10 +37,10 @@ public class AuthenticationController {
             authResponse.setSuccess(true);
             return ResponseEntity.ok(authResponse);
         } catch (UserAlreadyExistException e) {
-            authResponse.setSuccess(false);
-            authResponse.setFailureReason("USER_ALREADY_EXISTS");
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(authResponse);
-        } catch (EmailFailureException e) {
+        authResponse.setSuccess(false);
+        authResponse.setFailureReason("USER_ALREADY_EXISTS");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(authResponse);
+    } catch (EmailFailureException e) {
             log.info(e.getMessage());
             authResponse.setSuccess(false);
             authResponse.setFailureReason("EMAIL_SEND_FAILURE");
