8b614447a28291de9ab99414fa4116e52e5e9d11
Lev <lev.lvi2@gmail.com>
2024-07-18 20:26:18 +0300

    Fix code with uses @AuthenticationPrincipal

--- src/main/java/com/gameshop/ecommerce/user/controller/MyAccountController.java
+++ src/main/java/com/gameshop/ecommerce/user/controller/MyAccountController.java
@@ -1,40 +1,28 @@
 package com.gameshop.ecommerce.user.controller;
 
+import com.gameshop.ecommerce.user.model.LocalUser;
 import com.gameshop.ecommerce.user.model.LocalUserDto;
 import com.gameshop.ecommerce.user.service.MyAccountService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
 
 
-@Slf4j
 @RestController
-@RequestMapping("/my_account")
+@RequestMapping("/account")
 @RequiredArgsConstructor
 @Tag(name = "MyAccount", description = "API to work with My Account page")
 public class MyAccountController {
 
     private final MyAccountService myAccountService;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<LocalUserDto> getInfo(@PathVariable UUID userId){
-        return ResponseEntity.ok(myAccountService.getInfo(userId));
-    }
-
-    @PutMapping("/{userId}")
-    public ResponseEntity<LocalUserDto> updateInfo(@PathVariable UUID userId, @RequestBody LocalUserDto userDto){
-        LocalUserDto updatedUser = myAccountService.updateInfo(userId, userDto);
+    @PatchMapping
+    public ResponseEntity<LocalUserDto> updateInfo(@AuthenticationPrincipal LocalUser user, @RequestBody LocalUserDto userDto) throws Exception {
+        LocalUserDto updatedUser = myAccountService.updateInfo(user, userDto);
         return ResponseEntity.ok(updatedUser);
     }
 
-    @DeleteMapping("/{userId}")
-    public ResponseEntity<Void> deleteInfo(@PathVariable UUID userId) {
-        myAccountService.deleteInfo(userId);
-        return ResponseEntity.noContent().build();
-    }
 }
--- src/main/java/com/gameshop/ecommerce/user/service/MyAccountService.java
+++ src/main/java/com/gameshop/ecommerce/user/service/MyAccountService.java
@@ -2,11 +2,13 @@ package com.gameshop.ecommerce.user.service;
 
 import com.gameshop.ecommerce.address.Address;
 import com.gameshop.ecommerce.address.dto.AddressDto;
+import com.gameshop.ecommerce.exception.UserNotFoundException;
 import com.gameshop.ecommerce.security.EncryptionService;
 import com.gameshop.ecommerce.user.dao.LocalUserDAO;
 import com.gameshop.ecommerce.user.model.LocalUser;
 import com.gameshop.ecommerce.user.mapper.LocalUserMapper;
 import com.gameshop.ecommerce.user.model.LocalUserDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class MyAccountService {
 
     private final LocalUserDAO localUserDAO;
@@ -23,93 +26,21 @@ public class MyAccountService {
 
     private final EncryptionService encryptionService;
 
-    @Autowired
-    public MyAccountService(LocalUserDAO localUserDAO, LocalUserMapper localUserMapper, EncryptionService encryptionService) {
-        this.localUserDAO = localUserDAO;
-        this.localUserMapper = localUserMapper;
-        this.encryptionService = encryptionService;
-    }
 
-    public LocalUserDto getInfo(UUID userId){
-        LocalUser user = localUserDAO.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        return userBuilder(user);
-    }
-
-    public void deleteInfo(UUID userId){
-        localUserDAO.deleteById(userId);
-    }
-
-    public LocalUserDto updateInfo(UUID userId, LocalUserDto userDto){
-        LocalUser user = localUserDAO.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        if(!user.getFirstName().equals(userDto.getFirstName())||
-                !user.getLastName().equals(userDto.getLastName())||
-                !user.getPhoneNumber().equals(userDto.getPhoneNumber())||
-                !user.getEmail().equals(userDto.getEmail())) {
-            if(userDto.getFirstName() != null){
-                user.setFirstName(userDto.getFirstName());
-            }else {
-                user.setFirstName(user.getFirstName());
-            }
-            if(userDto.getLastName() != null){
-                user.setLastName(userDto.getLastName());
-            }else{
-                user.setLastName(user.getLastName());
-            }
-            if(userDto.getPhoneNumber() != null){
-                user.setPhoneNumber(userDto.getPhoneNumber());
-            }else{
-                user.setPhoneNumber(user.getPhoneNumber());
-            }
-            if(userDto.getEmail() != null){
-                user.setEmail(userDto.getEmail());
-            }else{
-                user.setEmail(user.getEmail());
-            }
-            if(userDto.getNewPassword() == null){
-                LocalUser updatedUser = localUserDAO.save(user);
-                log.info("Information about user was updated successfully");
-                return localUserMapper.entityToDto(updatedUser);
-            } else{
-                return updatePassword(user,userDto);
-            }
-
-        }
-        else{
-            user.setFirstName(user.getFirstName());
-            user.setLastName(user.getLastName());
-            user.setPhoneNumber(user.getPhoneNumber());
-            user.setEmail(user.getEmail());
-            if(userDto.getNewPassword() == null){
-                LocalUser updatedUser = localUserDAO.save(user);
-                return localUserMapper.entityToDto(updatedUser);
-            }else{
-                log.info("No new information. Checking password");
-                return updatePassword(user,userDto);
+    public LocalUserDto updateInfo(LocalUser user, LocalUserDto userDto) throws Exception {
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        user.setEmail(user.getEmail());
+        if(userDto.getNewPassword() != null) {
+            if (encryptionService.checkPassword(userDto.getOldPassword(), user.getPassword())) {
+                user.setPassword(encryptionService.encryptPassword(userDto.getNewPassword()));
+            } else {
+                throw new Exception("Incorrect old password");
             }
         }
+        LocalUser updatedUser = localUserDAO.save(user);
+        return localUserMapper.entityToDto(updatedUser);
     }
 
-    public LocalUserDto updatePassword(LocalUser user, LocalUserDto userDto){
-        if(encryptionService.checkPassword(userDto.getOldPassword(), user.getPassword())){
-            user.setPassword(encryptionService.encryptPassword(userDto.getNewPassword()));
-            LocalUser updatedUser = localUserDAO.save(user);
-            return localUserMapper.entityToDto(updatedUser);
-        }else{
-            log.info("Old password is incorrect");
-            LocalUser updatedUser = localUserDAO.save(user);
-            return localUserMapper.entityToDto(updatedUser);
-        }
-    }
-    public LocalUserDto userBuilder(LocalUser user) {
-        return LocalUserDto.builder()
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .phoneNumber(user.getPhoneNumber())
-                .email(user.getEmail())
-                .build();
-    }
 }
